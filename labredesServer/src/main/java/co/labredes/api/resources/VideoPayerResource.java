d4bc08196cd0f00abccf1899c1e411e65e418b43
cis321 <care_nalga01@hotmail.com>
2015-10-11 11:35:37 -0500

    add upload file

--- labredesServer/src/main/java/co/labredes/api/resources/VideoPayerResource.java
+++ labredesServer/src/main/java/co/labredes/api/resources/VideoPayerResource.java
@@ -1,14 +1,19 @@
 package co.labredes.api.resources;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.sun.jersey.multipart.FormDataParam;
 
 import co.labredes.domain.business.VideoPlayerBusiness;
 import co.labredes.infrastruncture.exceptions.IException;
@@ -49,6 +54,29 @@ public class VideoPayerResource {
         }
 
         return response;
+    }
+    
+    @POST
+    @Path("/upload/{videoName}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadVideo(@FormDataParam("videoFile") InputStream videoFile, @PathParam("videoName") String videoName){
+
+    	Response response = null;
+    	
+    	Either<IOException, String> either = videoPlayerBusiness.uploadVideo(videoFile,videoName);
+    	
+    	if (either.isRight()){
+    		
+    		response = Response.status(200).entity(either.right().value()).build();
+    		
+    	} else {
+    		
+    		response = Response.status(500).entity(either.left().value().getMessage()).build();
+    		
+    	}
+    	
+    	return response;
     }
 
 }
